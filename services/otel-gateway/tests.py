57209a56e1e866ca8951ad41e67d0fcd84518edd
bloodearnest <simon.davy@thedatalab.org>
2022-10-26 16:54:46 +0100

    add otel-gateway deploy workflow

--- services/otel-gateway/tests.py
+++ services/otel-gateway/tests.py
@@ -3,22 +3,43 @@ import os
 import time
 from pathlib import Path
 
-import generate
+from opentelemetry import metrics, trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import \
+    OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+
 import pytest
 
 trace_file = Path("exported/traces.json")
 metric_file = Path("exported/metrics.json")
 
+# set up trace exporting
+provider = TracerProvider()
+provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter()))
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer("testscope")
+
+
+def generate_test_trace():
+    """Fixture to generate trace data."""
+    with tracer.start_as_current_span("testname") as span:
+        span.set_attribute("testattr", "testvalue")
+
+
+# Currently, python sdk can only export metrics over grpc, not http. And our
+# collector is http only atm. Once the following pull request lands then we can
+# add a test for metrics too.
+# https://github.com/open-telemetry/opentelemetry-python/pull/2891
+def generate_test_metric():
+    """Fixture to generate metric data"""
+    pass
+
+
 
 @pytest.fixture(autouse=True)
 def clear_files():
-    # ensure test server has stasted up
-    #while not trace_file.exists():
-    #    time.sleep(0.01)
-    #while not metric_file.exists():
-    #    time.sleep(0.01)
-
-    # server has fileno already, so we can't delete
+    # ensure clean data
     if trace_file.exists():
         os.truncate(str(trace_file), 0)
     if metric_file.exists():
@@ -26,7 +47,6 @@ def clear_files():
 
 
 def get_output(path):
-
     # wait for file to be written to, typically a few hundered 100ms
     while path.exists() and path.stat().st_size == 0:
         time.sleep(0.01)
@@ -35,7 +55,7 @@ def get_output(path):
 
 
 def test_trace():
-    generate.generate_trace()
+    generate_test_trace()
 
     output = get_output(trace_file)
     spans = output["resourceSpans"]
@@ -57,3 +77,14 @@ def test_trace():
     assert span_data["name"] == "testname"
     assert span_data["attributes"][0]["key"] == "testattr"
     assert span_data["attributes"][0]["value"] == {"stringValue": "testvalue"}
+
+
+if __name__ == "__main__":
+    print("Generating trace data")
+    generate_test_trace()
+    # print("Generating metric data")
+    # generate_test_metric()
+
+
+
+
